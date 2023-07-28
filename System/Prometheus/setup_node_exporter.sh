80c7d9f316c45cfd6a713de3872078285ced116a
NinTech <nintech-sudo@noreply.github.com>
2023-07-28 22:39:53 +0700

    update file

--- System/Prometheus/setup_node_exporter.sh
+++ System/Prometheus/setup_node_exporter.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-wget https://github.com/nintech-sudo/studydevsysops/raw/main/System/Prometheus/node_exporter-1.0.1.linux-amd64.tar.gz
+wget https://github.com/nintech-studydevsysops/raw/main/System/Prometheus/node_exporter-1.0.1.linux-amd64.tar.gz
 
 # Create User
-sudo groupadd -f node_exporter
-sudo useradd -g node_exporter --no-create-home --shell /bin/false node_exporter
-sudo mkdir /etc/node_exporter
-sudo chown node_exporter:node_exporter /etc/node_exporter
+groupadd -f node_exporter
+useradd -g node_exporter --no-create-home --shell /bin/false node_exporter
+mkdir /etc/node_exporter
+chown node_exporter:node_exporter /etc/node_exporter
 
 # Unpack Node Exporter Binary
 tar -xvf node_exporter-1.0.1.linux-amd64.tar.gz
 mv node_exporter-1.0.1.linux-amd64 node_exporter-files
 
 # Install Node Exporter
-sudo cp node_exporter-files/node_exporter /usr/bin/
-sudo chown node_exporter:node_exporter /usr/bin/node_exporter
+cp node_exporter-files/node_exporter /usr/bin/
+chown node_exporter:node_exporter /usr/bin/node_exporter
 
 #Setup Node Exporter Service
 cat << EOF > /usr/lib/systemd/system/node_exporter.service
@@ -35,11 +35,11 @@ ExecStart=/usr/bin/node_exporter --web.listen-address=:9100
 WantedBy=multi-user.target
 EOF
 
-sudo chmod 664 /usr/lib/systemd/system/node_exporter.service
-sudo systemctl daemon-reload
-sudo systemctl start node_exporter
-sudo systemctl enable node_exporter.service
-sudo systemctl status node_exporter
+chmod 664 /usr/lib/systemd/system/node_exporter.service
+systemctl daemon-reload
+systemctl start node_exporter
+systemctl enable node_exporter.service
+systemctl status node_exporter
 
 
 echo "Please check firewall and open port 9100"
